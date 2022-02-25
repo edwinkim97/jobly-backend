a1572f6f191e75484f7440a111611787fe18dbcf
edwinkim97 <edwinkim124@gmail.com>
2022-02-24 16:19:32 -0800

    working on building jobs model

--- models/_testCommon.js
+++ models/_testCommon.js
@@ -8,6 +8,8 @@ async function commonBeforeAll() {
   await db.query("DELETE FROM companies");
   // noinspection SqlWithoutWhere
   await db.query("DELETE FROM users");
+  
+  await db.query("DELETE FROM jobs");
 
   await db.query(`
     INSERT INTO companies(handle, name, num_employees, description, logo_url)
@@ -28,6 +30,15 @@ async function commonBeforeAll() {
       await bcrypt.hash("password1", BCRYPT_WORK_FACTOR),
       await bcrypt.hash("password2", BCRYPT_WORK_FACTOR),
     ]);
+
+  await db.query(`
+    INSERT INTO jobs(title,
+                    salary,
+                    equity,
+                    company_handle)
+    VALUES ('computer programmer', 200000, 0, 'c1'),
+         ('mechanical engineer', 100000, .3, 'c2'),
+         ('musician', null, null, 'c2')`);
 }
 
 async function commonBeforeEach() {
